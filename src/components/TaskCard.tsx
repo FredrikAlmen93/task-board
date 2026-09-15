c28366e8fd0f2944e000f5a43dd6716b06d8e725
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-09-15 13:20:20 +0200

    TaskCard.tsx created

---
+++ src/components/TaskCard.tsx
@@ -0,0 +1,12 @@
+function  TaskCard () {
+    return (
+        <article>
+            <p>Frontend</p>
+            <h3>Skapa dashboard</h3>
+            <p>Bygg en enkel dashboard med React.</p>
+            <p>Ansvarig: Fredrik</p>
+            <p>Prioritet: Hög</p>
+        </article>
+    );
+}
+export default TaskCard;
