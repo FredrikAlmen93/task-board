1d45bd8c438eb1c8521b43bb826ddd6176ef7b39
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-09-15 13:25:48 +0200

    implemented taskcard into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import TaskCard from "./components/TaskCard";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
     <Header />
     <main>
       <h2>Task-Board</h2>
+      <TaskCard title="Skapa Dashboard" />
       </main>
       <Footer />
       </>
