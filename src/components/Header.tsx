743192ef67af66eee30f985bb538e864cc8577b8
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-09-15 13:11:09 +0200

    Header.tsx created

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header () {
+    return (
+        <header>
+            <p>Labb del 1</p>
+            <h1>Task-Board</h1>
+            <p>Ett enkelt task-board byggd med react och TypeScript</p>
+        </header>
+    );
+}
+export default Header;
