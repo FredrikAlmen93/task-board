f9384644f30e9db1105575137aa9093ff0025b60
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-09-15 14:47:17 +0200

    Tasck cards added to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,43 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import TaskCard from "./components/TaskCard";
+import Column from "./components/Column";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <h2>Task-Board</h2>
-        <TaskCard
-          title="Skapa Dashboard"
-          id={1}
-          description="Bygg en enkel dashboard med React."
-          assignee="Fredrik"
-          category="Frontend"
-          priority="Hög"
-        />
-        <TaskCard
-          title="Skapa design"
-          id={2}
-          description="Bestäm färger och layout för applikationen."
-          assignee="Erik"
-          category="Design"
-          priority="Medium"
-        />
-        <TaskCard
-          title="Skriva tester"
-          id={3}
-          description="Skriv tester för applikationens komponenter."
-          assignee="Miranda"
-          category="Testning"
-          priority="Låg"
-        />
+        <div>
+          <Column title="To-Do">
+            <TaskCard
+              title="Skapa Dashboard"
+              id={1}
+              description="Bygg en enkel dashboard med React."
+              assignee="Fredrik"
+              category="Frontend"
+              priority="Hög"
+            />
+          </Column>
+          <Column title="In Progress">
+          <TaskCard
+            title="Skapa design"
+            id={2}
+            description="Bestäm färger och layout för applikationen."
+            assignee="Erik"
+            category="Design"
+            priority="Medium"
+          /></Column>
+          <Column title="Done">
+          <TaskCard
+            title="Skapa backend"
+            id={3}
+            description="Bygg en enkel backend med Node.js."
+            assignee="Anna"
+            category="Backend"
+            priority="Låg"
+          /></Column>
+        </div>
       </main>
       <Footer />
     </>
