cfaed7e7eee8fe8b459b6ca55ecbbdbf18c2136c
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-09-16 19:33:26 +0200

    Add Navigation component to App for improved task board navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
 import TaskBoardPage from "./pages/TaskBoardPage";
 import NewTaskPage from "./pages/NewTaskPage";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header />
+      <Navigation />
       <Routes>
         <Route path="/" element={<TaskBoardPage />} />
         <Route path="/create" element={<NewTaskPage />} />
