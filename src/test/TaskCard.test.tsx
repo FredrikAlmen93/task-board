062f01db832e84af4aa6169d6286154537cd2902
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-09-16 14:31:25 +0200

    TaskCard tests created

---
+++ src/test/TaskCard.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import TaskCard from "../components/TaskCard";
+
+describe("TaskCard", () => {
+  it("visar information från props", () => {
+    render(
+      <TaskCard
+        title="Testa TaskCard"
+        id={1}
+        description="Detta är en testuppgift"
+        assignee="Fredrik"
+        category="Testkategori"
+        priority="Hög"
+      />,
+    );
+
+    expect(screen.getByText("Testa TaskCard")).toBeInTheDocument();
+    expect(screen.getByText("Detta är en testuppgift")).toBeInTheDocument();
+    expect(screen.getByText("Ansvarig: Fredrik")).toBeInTheDocument();
+    expect(screen.getByText("Testkategori")).toBeInTheDocument();
+    expect(screen.getByText("Prioritet: Hög")).toBeInTheDocument();
+  });
+});
